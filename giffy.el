dba1312433bdfcf643b2160193e7217e4ffb9921
Lars Ingebrigtsen <larsi@gnus.org>
2017-10-08 13:13:24 +0200

    Scaffolding

---
+++ giffy.el
@@ -0,0 +1,32 @@
+;;; giffy.el --- creating GIF animations
+;; Copyright (C) 2017 Lars Magne Ingebrigtsen
+
+;; Author: Lars Magne Ingebrigtsen <larsi@gnus.org>
+;; Keywords: fun, images
+
+;; This file is not part of GNU Emacs.
+
+;; giffy.el is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 2, or (at your option)
+;; any later version.
+
+;; giffy.el is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.	 See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs; see the file COPYING.  If not, write to the
+;; Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+;; Boston, MA 02111-1307, USA.
+
+;;; Commentary:
+
+;;; Code:
+
+(require 'cl)
+
+(provide 'giffy)
+
+;;; giffy.el ends here
